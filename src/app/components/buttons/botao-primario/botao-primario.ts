caa5128e64cd20ead610ce199a3c54e90501fa36
RianFonsecaa <rian.sfonsecaa@gmail.com>
2025-08-26 22:06:41 -0300

    refactor: Componentização dos Inputs e Botões das telas de Login e Registro

---
+++ src/app/components/buttons/botao-primario/botao-primario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botao-primario',
+  imports: [],
+  templateUrl: './botao-primario.html',
+  styleUrl: './botao-primario.css'
+})
+export class BotaoPrimario {
+
+}
--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { InputBase } from '../../components/input-base/input-base';
+import { BotaoGoogle } from '../../components/buttons/botao-google/botao-google';
+import { BotaoPrimario } from '../../components/buttons/botao-primario/botao-primario';
 
 @Component({
   selector: 'app-login-page',
   standalone: true, // <-- ESSA LINHA É CRÍTICA!
-  imports: [],
+  imports: [InputBase, BotaoGoogle, BotaoPrimario],
   templateUrl: './login-page.html',
 })
-export class LoginPage {
-
-}
+export class LoginPage {}
