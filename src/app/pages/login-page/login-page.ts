d7bea50ca2e5d02d79ba599188d7ce614c412714
YuriPepezin <pepeyuri2003@gmail.com>
2025-08-26 23:02:26 -0300

    refactor: componente plano de fundo
    plano de fundo com bordas brancas

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { InputBase } from '../../components/input-base/input-base';
 import { BotaoGoogle } from '../../components/buttons/botao-google/botao-google';
 import { BotaoPrimario } from '../../components/buttons/botao-primario/botao-primario';
-
+import {PlanoDeFundo} from '../../components/plano-de-fundo/plano-de-fundo';
 @Component({
   selector: 'app-login-page',
   standalone: true, // <-- ESSA LINHA É CRÍTICA!
-  imports: [InputBase, BotaoGoogle, BotaoPrimario],
+  imports: [InputBase, BotaoGoogle, BotaoPrimario, PlanoDeFundo],
   templateUrl: './login-page.html',
 })
 export class LoginPage {}
